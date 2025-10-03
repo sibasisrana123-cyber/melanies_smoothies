ab71a49bdebdbe934053434e0d5238c257a4c850
sibasisrana123-cyber <sibasisrana123@gmail.com>
2025-10-03 14:14:03 +0530

    Update streamlit_app.py
    New section to display smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,14 @@ if ingredients_list:
    st.write(my_insert_stmt)
    time_to_insert = st.button('Sumbit Order')
 
-
-   if time_to_insert:
+   
+  if time_to_insert:
       session.sql(my_insert_stmt).collect()
        
       st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
