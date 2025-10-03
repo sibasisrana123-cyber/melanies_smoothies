ae5fe0391f5c6ebf13f6e9b936283c9193873d07
sibasisrana123-cyber <sibasisrana123@gmail.com>
2025-10-03 11:48:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,13 +29,13 @@ if ingredients_list:
 
    #st.write(ingredients_string)
 
-   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+   #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            #values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
    #st.write(my_insert_stmt)
    #st.stop()
 
-   st.write(my_insert_stmt)
+   #st.write(my_insert_stmt)
    time_to_insert = st.button('Sumbit Order')
 
 
