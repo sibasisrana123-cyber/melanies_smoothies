822f63eaff20d6bf8a56694083a82d21d0c1f41c
sibasisrana123-cyber <sibasisrana123@gmail.com>
2025-10-06 10:34:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,8 +40,8 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        #sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
 
 
    #st.write(ingredients_string)
